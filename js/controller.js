f9ba0b99f7794f4661b8064c7d868eac53f809d1
justRishi <justRishi@gmail.com>
2015-10-27 18:27:31 +0100

    commit with bugs by adding ElementGroupServicew

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,12 @@
 var HmisReport = angular.module('HmisReportCtrl', []);			
 			
 
-HmisReport.controller('HmisReportCtrl', ['$scope', 'ElementGroup', function($scope, ElementGroup){
+HmisReport.controller('HmisReportCtrl', ['$scope', 'Elements', function($scope, Elements){
 	$scope.hmisTitle = 'DIAGNOSTIC - DOSSIER';
-	$scope.elementGroup = ElementGroup.get({elementGrpId:'Utm69ZvS4op'});
+	$scope.elements = Elements.get({elementGrpId:'Utm69ZvS4op'});
+}]);
+
+HmisReport.controller('HmisReportCtrl', ['$scope', 'ElementsGrps', function($scope, ElementsGrps){
+	$scope.hmisTitle = 'DIAGNOSTIC - DOSSIER';
+	$scope.elementGroups = ElementsGrps.query();
 }]);
--- js/services.js
+++ js/services.js
@@ -1,13 +1,17 @@
-var qryElementGrps = 'http://localhost:8989/dhis/api/dataElementGroups/:elementGrpId.json?fields=dataElements[name,description]';
+var qryElementGrps = 'http://localhost:8989/dhis/api/dataElementGroups.json?paging=false&fields=[name,id]';
+var qryElements = 'http://localhost:8989/dhis/api/dataElementGroups/:elementGrpId.json?fields=dataElements[name,description]';
 var hmisReportServices = angular.module('hmisReportServices', ['ngResource']);
 var elementGrpId =  'Utm69ZvS4op';
 
-hmisReportServices.factory('ElementGroup', ['$resource',
+hmisReportServices.factory('Elements', ['$resource',
 function($resource){
-	return $resource(qryElementGrps,{elementGrpId:'@elementGrpId'},{
+	return $resource(qryElements,{elementGrpId:'@elementGrpId'},{
 		query: {method:'GET', isArray:true}
 	});
 }]);
 
-
+hmisReportServices.factory('ElementsGrps', ['$resource',
+function($resource){
+	return $resource(qryElementGrps);
+}]);
 
