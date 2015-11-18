5f86ddedae61161f3135ffeb28480cc75a98e4e9
justRishi <justRishi@gmail.com>
2015-11-18 12:33:32 +0100

    add select and show data elements of selected group

--- js/controller.js
+++ js/controller.js
@@ -1,12 +1,21 @@
 var HmisReport = angular.module('HmisReportCtrl', []);	
-var elementGrpId =  'DZmppihKF1V';
+//var elementGrpId =  'DZmppihKF1V';
 
 			
 
 HmisReport.controller('HmisReportCtrl', ['$scope', 'Elements', 'ElementsGrps',function($scope, Elements, ElementsGrps){
 	$scope.hmisTitle = 'DIAGNOSTIC - DOSSIER';
-	$scope.elements = Elements.get({elementGrpId:elementGrpId});
 	$scope.elementGroups = ElementsGrps.get();
+
+	$scope.getElement = function(){
+		$scope.elements = Elements.get({elementGrpId:$scope.selected.id});
+	};
+
+	$scope.setTitle = function(elementGroupName){
+		$scope.hmisTitle = elementGroupName;
+		alert(elementGroupName);
+	};
+
 }]);
 
 /*
