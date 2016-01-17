9778f05ae451a248204f0aa9a98fe89eb941e8fe
justRishi <justRishi@gmail.com>
2016-01-17 23:16:10 +0100

    added dossiertext but with bug

--- js/controller.js
+++ js/controller.js
@@ -1,8 +1,6 @@
-var HmisReport = angular.module('HmisReportCtrl', []);	
+var HmisReport = angular.module('HmisReportCtrl',['ngSanitize']);
 
-			
-
-HmisReport.controller('HmisReportCtrl', ['$scope', 'Elements', 'ElementsGrps','DataSets', 'DataSet', 'ElementsInSection', function($scope, Elements, ElementsGrps, DataSets, DataSet, ElementsInSection){
+HmisReport.controller('HmisReportCtrl', ['$scope','$sce','Elements', 'ElementsGrps','DataSets', 'DataSet', 'ElementsInSection', 'DossierValue', function($scope, $sce, Elements, ElementsGrps, DataSets, DataSet, ElementsInSection, DossierValue){
 	$scope.hmisTitle = '';
 	$scope.elementGroups = ElementsGrps.get();
 	$scope.dataSets = DataSets.get();
@@ -12,18 +10,16 @@ HmisReport.controller('HmisReportCtrl', ['$scope', 'Elements', 'ElementsGrps','D
 	};
 
 	$scope.getDataSections = function(){
+		$scope.dossierRow = {};
+
 		$scope.dataset = DataSet.get({dataSetId:$scope.selectedSet.id});
+		$scope.dossierRow = DossierValue.get({dataSetId:$scope.selectedSet.code});		
+		//console.log("selected set" + $("#drpBoxDataSet").val());
+		console.log("selected code " + $scope.selectedSet.code);
 	};
 
 	$scope.getElementsSection = function(sectionId){
 		return $scope.sectionElements = ElementsInSection.get({sectionId:sectionId});
+		//$scope.elements = Elements.get({elementGrpId:$scope.selected.id});
 	};
-
 }]);
-
-/*
-HmisReport.controller('HmisReportCtrl', ['$scope', 'ElementsGrps', function($scope, ElementsGrps){
-	$scope.elementGroups = ElementsGrps.get();
-}]);
-
-*/
--- js/services.js
+++ js/services.js
@@ -1,8 +1,9 @@
 var qryElementGrps = 'http://localhost:8989/dhis/api/dataElementGroups.json?paging=false&fields=[name,id]';
 var qryElements = 'http://localhost:8989/dhis/api/dataElementGroups/:elementGrpId.json';
-var qryDataSets = 'http://localhost:8989/dhis/api//dataSets.json?fields=id,displayName&paging=false&translate=true';
+var qryDataSets = 'http://localhost:8989/dhis/api//dataSets.json?fields=id,displayName,code&paging=false&translate=true';
 var qryDataSet = 'http://localhost:8989/dhis/api/dataSets/:dataSetId.json';
 var qryDataElemenetsInSection = 'http://localhost:8989/dhis/api/sections/:sectionId/dataElements.json';
+var qryDossierValue = 'http://localhost:8989/dhis/api/sqlViews/FbUqezq7Aqp/data?criteria=code::dataSetCode';
 
 var hmisReportServices = angular.module('hmisReportServices', ['ngResource']);
 
@@ -42,9 +43,13 @@ function($resource){
     });
 }]);
 
-/* 
-x 1. Get datasection names and show in a list
-  2. For each datasection show the elements
-  2.1 foreach   https://docs.angularjs.org/api/ng/function/angular.forEach
-  3. Add data section for long text.
-*/
+hmisReportServices.factory('DossierValue', ['$resource',
+function($resource){
+  console.log("qry DossierValue = " + qryDossierValue);
+  return $resource(qryDossierValue, {dataSetCode:'@dataSetCode'}, {
+      query: {method:'GET',  isArray:true}
+    });
+}]);
+
+
+
