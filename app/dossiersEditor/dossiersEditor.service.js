ca4d0177ec6da4385e4ebbb3a48c98d884f5ab87
Bruno Raimbault <raimbault.bruno@gmail.com>
2016-08-29 13:11:22 +0200

    0.7.5-dev
    - added POST method to create new descriptions in dossiersEditor
    - fixed possible breach in dossierEditor (via url) by hidding save
    button whe user does not have authorization

--- app/dossiersEditor/dossiersEditor.service.js
+++ app/dossiersEditor/dossiersEditor.service.js
@@ -5,6 +5,7 @@
 
 var qryTranslation = dhisUrl + 'translations.json?fields=id,[className,locale,objectId,property]&filter=className\\:eq\\:OrganisationUnitGroup&filter=property\\:eq\\:description&filter=locale\\:eq\\::languageCode&filter=objectId\\:eq\\::serviceId&paging=false';
 var qryUpdateDescription = dhisUrl + 'translations/:translationId';
+var qryCreateDescription = dhisUrl + 'translations/';
 
 dossiersEditorModule.factory('dossiersEditorTranslationFactory', ['$resource',
     function($resource) {
@@ -27,6 +28,17 @@ dossiersEditorModule.factory('dossiersEditorUpdateDescriptionFactory', ['$resour
         }, {
             update: {
                 method: 'PATCH'
+            }
+        });
+    }
+]);
+
+dossiersEditorModule.factory('dossiersEditorCreateDescriptionFactory', ['$resource',
+    function($resource) {
+        return $resource(qryCreateDescription, {
+        }, {
+            create: {
+                method: 'POST'
             }
         });
     }
