4d04b189c553c3cd49e1c73a5561120b8e866c77
Bruno RAIMBAULT <bruno.raimbault@barcelona.msf.org>
2016-11-30 11:07:40 +0100

    v0.8.3 update dependencies and licenses

--- deploy_HMIS_Dictionary.bat
+++ deploy_HMIS_Dictionary.bat
@@ -1,1 +1,1 @@
-xcopy /s "D:\GitHub\HMIS Dictionary" "C:\Program Files (x86)\DHIS2\DHIS\apps\dhis2Reports"
+xcopy /s "D:\GitHub\HMIS_Dictionary" "C:\Program Files (x86)\DHIS2\DHIS\apps\dhis2Reports"
